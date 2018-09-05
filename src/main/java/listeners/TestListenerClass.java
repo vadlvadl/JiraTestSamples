b2ee8f831502528e0a21fbafd080d7aa44407f99
Vadim Lizogub <vadlvadl789@gmail.com>
2018-09-05 10:31:19 +0300

    TestListenerClass added screenshot method

--- src/main/java/listeners/TestListenerClass.java
+++ src/main/java/listeners/TestListenerClass.java
@@ -1,10 +1,20 @@
 package listeners;
 
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class TestListenerClass implements ITestListener {
+
+    @Attachment
+    public byte[] captureScreenshot(WebDriver d) {
+        return ((TakesScreenshot) d).getScreenshotAs(OutputType.BYTES);
+    }
+
     @Override
     public void onTestStart(ITestResult iTestResult) {
         System.out.println("Test has been started");
@@ -18,6 +28,11 @@ public class TestListenerClass implements ITestListener {
     @Override
     public void onTestFailure(ITestResult iTestResult) {
         System.out.println("Test FAILURE");
+        Object webDriverAttribute = iTestResult.getTestContext().getAttribute("WebDriver");
+        if (webDriverAttribute instanceof WebDriver) {
+            System.out.println("Screesnshot captured for test case:" + iTestResult.getMethod().getMethodName());
+            captureScreenshot((WebDriver) webDriverAttribute);
+        }
     }
 
     @Override
